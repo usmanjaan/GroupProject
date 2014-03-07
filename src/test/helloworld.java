e8b5e8d30d913eeda8fb1a46396dd39cd58c6875
mccarthyeoin <eoinmccarthy17@hotmail.com>
2014-03-07 15:42:16 +0000

    Changed a line
    change

--- src/test/helloworld.java
+++ src/test/helloworld.java
@@ -4,7 +4,10 @@ public class helloworld {
 	
 	public static void main(String args[])
 	{
-		System.out.println("Hello World");
+		System.out.println("Goodbye");
+		System.out.println("Hello");
+
+		
 	}
 
 }
