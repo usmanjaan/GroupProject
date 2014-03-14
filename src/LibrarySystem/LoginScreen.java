2ed585c239fc150ae0d929209b4d31e0f1542cc4
mccarthyeoin <eoinmccarthy17@hotmail.com>
2014-03-14 14:02:55 +0000

    Made changes

--- src/LibrarySystem/LoginScreen.java
+++ src/LibrarySystem/LoginScreen.java
@@ -1,13 +1,13 @@
 package LibrarySystem;
 
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
 
 public class LoginScreen extends JPanel {
 	
@@ -15,14 +15,22 @@ public class LoginScreen extends JPanel {
 	JTextField pass = new JTextField(20);
 	JLabel lname = new JLabel("Name");
 	JLabel lpass = new JLabel("Password");
+	JLabel title = new JLabel("Login Page");
+
 	
 	public LoginScreen(){
 		//this.setBackground(Color.magenta);
-		this.setLayout(new GridLayout(3,2));
-		this.add(lname);
-		this.add(name);
-		this.add(lpass);
-		this.add(pass);
+		//this.setLayout(new GridLayout(3,2));
+		//this.setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
+		//this.add(title);
+		//this.add(lpass);
+		//this.add(pass);
+		//this.add(lname);
+		//this.add(name);
+		//this.add(lname, BorderLayout.CENTER);
+		//this.add(name);
+		//this.add(lpass);
+		//this.add(pass);
 	
 		//this.setSize(100, 100);
 		this.setPreferredSize(new Dimension(300, 50));
--- src/LibrarySystem/SystemFrame.java
+++ src/LibrarySystem/SystemFrame.java
@@ -1,14 +1,12 @@
 package LibrarySystem;
 
-import java.awt.GridBagLayout;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class SystemFrame extends JFrame {
 	
-	LoginScreen scrn = new LoginScreen();
+	LoginScreen scrn;
 	JLabel txt = new JLabel("Text");
 	JPanel pane = new JPanel();
 	JPanel pane1 = new JPanel();
@@ -16,7 +14,8 @@ public class SystemFrame extends JFrame {
 	public SystemFrame(){
 		this.setSize(500, 500);
 		this.setLocation(300, 300);
-		this.setLayout(new GridBagLayout());
+		scrn = new LoginScreen();
+		//this.setLayout(new GridBagLayout());
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.getContentPane().add(scrn);
 		//this.add(txt);
